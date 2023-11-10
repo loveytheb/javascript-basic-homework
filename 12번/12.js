8f8c87bd791e2a7658eb164522d139393e4b86d4
loveytheb <cjstjsn99@gamil.com>
2023-11-10 16:42:49 +0900

    3회차 과제 완료

---
+++ 12번/12.js
@@ -0,0 +1,14 @@
+const 투두 = {
+  id: 1,
+  할일: "8시기상",
+  완료여부: false,
+};
+
+// 여기에 코드를 작성해주세요.
+const newTodo = {...투두, 완료여부: false,}
+
+console.log(투두)
+console.log(newTodo);
+// 결과
+// { id: 1, '할일': '8시기상', '완료여부': false }
+// { id: 1, '할일': '8시기상', '완료여부': true }
